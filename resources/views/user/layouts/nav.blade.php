1cd2f81383036b380a121834a8fcc80aabf0ad28
dasest404 <dasest404@gmail.com>
2017-12-09 19:01:26 +0530

    commit two

---
+++ resources/views/user/layouts/nav.blade.php
@@ -0,0 +1,67 @@
+      <!-- site branding -->
+      <div class="site-branding">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-3">
+              <ul class="social-icon">
+                <li><a href="" class="icon-twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="" class="icon-facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="" class="icon-instagram"><i class="fa fa-instagram"></i></a></li>
+                <li><a href="" class="icon-pinterest"><i class="fa fa-pinterest"></i></a></li>
+                <li><a href="" class="icon-youtube"><i class="fa fa-youtube-play"></i></a></li>
+              </ul>
+            </div><!-- .col-md-3 -->
+
+            <div class="col-md-6">
+              <!-- <h1 class="site-title"><a href="index.html" rel="home">Astrif.</a></h1> -->
+              <h1 class="site-title title-image"><a href="index.html" rel="home"><img src="{{ asset('user/img/astrif-logo.png')}}" alt="astrif."></a></h1>
+              <p class="site-description">- Good Food Lovers -</p>
+            </div><!-- .col-md-6 -->
+
+            <div class="col-md-3">
+              <form class="search-form">
+                <div class="form-group">
+                  <label for="search" class="sr-only">Search</label>
+                  <input type="text" class="form-control search-field" placeholder="Search...">
+                </div><!-- .form-group -->
+                <button type="submit" class="btn btn-default search-submit"><i class="fa fa-search"></i></button>
+              </form><!-- .search-form -->
+            </div><!-- .col-md-3 -->
+          </div><!-- .row -->
+        </div><!-- .container -->
+      </div><!-- .site-branding -->
+
+      <!-- main navigation -->
+      <div class="site-navigation-sticky-wrapper">
+        <header id="masthead" class="site-header" role="banner">
+          <nav id="site-navigation" class="main-navigation" role="navigation">
+            <div class="container">
+              <div class="row">
+                <div class="col-md-12">
+                  <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><i class="fa fa-bars"></i><span class="sr-only">Primary Menu</span></button>
+                  <div class="menu-container">
+                    <ul id="primary-menu" class="menu nav-menu" aria-expanded="false">
+                      <li class="menu-item current-menu-item"><a href="index.html">Home</a></li>
+                      <li class="menu-item menu-item-has-children" aria-haspopup="true"><a href="index.html#">Features</a>
+                        <ul class="sub-menu">
+                          <li class="menu-item"><a href="single.html">Single Post</a></li>
+                          <li class="menu-item"><a href="single-sidebar.html">Single Post With Sidebar</a></li>
+                          <li class="menu-item"><a href="page-fullwidth.html">Page Full Width</a></li>
+                          <li class="menu-item"><a href="page-sidebar.html">Page With Sidebar</a></li>
+                          <li class="menu-item"><a href="shortcodes.html">Shortcodes</a></li>
+                          <li class="menu-item"><a href="typography.html">Typography</a></li>
+                          <li class="menu-item"><a href="404.html">Not Found</a></li>
+                        </ul>
+                      </li>
+                      <li class="menu-item"><a href="category.html">Food</a></li>
+                      <li class="menu-item"><a href="category.html">Cook</a></li>
+                      <li class="menu-item"><a href="about.html">About Me</a></li>
+                      <li class="menu-item"><a href="contact.html">Contact</a></li>
+                    </ul>
+                  </div><!-- .menu-container -->
+                </div><!-- .col-md-12 -->
+              </div><!-- .row -->
+            </div><!-- .container -->
+          </nav><!-- #site-navigation -->
+        </header><!-- #masthead -->
+      </div><!-- .site-header-affix-wrapper -->
